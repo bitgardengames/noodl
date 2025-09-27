c40b832be0a581f1ab2347a37413d0dfa8ba8774
bitgardengames <bitgardengames@gmail.com>
2025-09-26 22:35:53 -0600

    Revert "Add floor-specific shaders for background ambiance"

--- arena.lua
+++ arena.lua
@@ -1,5 +1,4 @@
 local Theme = require("theme")
-local FloorShaders = require("floorshaders")
 
 local EXIT_SAFE_ATTEMPTS = 180
 local MIN_HEAD_DISTANCE_TILES = 2
@@ -110,11 +109,7 @@ function Arena:drawBackground()
 
     -- Solid fill
     love.graphics.setColor(Theme.arenaBG)
-    local usedShader = FloorShaders:apply("arena", love.graphics.getWidth(), love.graphics.getHeight())
     love.graphics.rectangle("fill", ax, ay, aw, ah)
-    if usedShader then
-        FloorShaders:clear()
-    end
 
     love.graphics.setColor(1, 1, 1, 1)
 end
