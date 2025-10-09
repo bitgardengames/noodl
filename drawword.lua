59a7e0b845102590133ea1af88899e56766e0fc6
BitGarden <bitgardengames@gmail.com>
2025-10-09 16:22:46 -0600

    Update drawword.lua

--- drawword.lua
+++ drawword.lua
@@ -1,74 +1,19 @@
 local drawSnake = require("snakedraw")
 
---[[local letters = {
-  n = {
-    {0,0}, {0,3}, {1,3}, {1,0}, {2,0}, {2,3}
-  },
-  o = {
-    {0,0}, {2,0}, {2,3}, {0,3}, {0,0}
-  },
-  d = {
-    {0,3}, {0,0}, {1,0}, {2,1}, {2,2}, {1,3}, {0,3}
-  },
-  l = {
-    {0,0}, {0,3}
-  }
-}]]
-
-local function expandLetter(points)
-  local expanded = {}
-  if not points or #points == 0 then
-    return expanded
-  end
-
-  local function addPoint(x, y)
-    local last = expanded[#expanded]
-    if not last or last[1] ~= x or last[2] ~= y then
-      expanded[#expanded + 1] = {x, y}
-    end
-  end
-
-  addPoint(points[1][1], points[1][2])
-
-  for i = 2, #points do
-    local prev = points[i - 1]
-    local curr = points[i]
-    local dx = curr[1] - prev[1]
-    local dy = curr[2] - prev[2]
-
-    assert(dx == 0 or dy == 0, "Letter paths must use axis-aligned segments")
-
-    local stepX = dx > 0 and 1 or (dx < 0 and -1 or 0)
-    local stepY = dy > 0 and 1 or (dy < 0 and -1 or 0)
-
-    local x, y = prev[1], prev[2]
-    while x ~= curr[1] or y ~= curr[2] do
-      if stepX ~= 0 then
-        x = x + stepX
-      else
-        y = y + stepY
-      end
-      addPoint(x, y)
-    end
-  end
-
-  return expanded
-end
-
 local letters = {
-  n = expandLetter({
-    {0, 2}, {0, 0}, {2, 0}, {2, 2}
-  }),
-  o = expandLetter({
-    {0, 0}, {2, 0}, {2, 2}, {0, 2}, {0, 0}
-  }),
-  d = expandLetter({
-    {0, 0}, {2, 0}, {2, 2}, {0, 2}, {0, 0},
-    {2, 0}, {2, -2}
-  }),
-  l = expandLetter({
-    {0, -2}, {0, 2}
-  })
+	n = {
+		{0,2}, {0,0},{2,0},{2,2}
+	},
+	o = {
+		{0,0}, {2,0}, {2,2}, {0,2}, {0,0}
+	},
+	d = {
+		{0,0}, {2,0}, {2,2}, {0,2}, {0,0},
+		{2,0}, {2,-2}
+	},
+	l = {
+		{0,-2}, {0,2}
+	}
 }
 
 local function drawWord(word, ox, oy, cellSize, spacing)
@@ -111,8 +56,6 @@ local function drawWord(word, ox, oy, cellSize, spacing)
       -- Disable the built-in face rendering here to avoid double faces.
       drawSnake(snakeTrail, #snakeTrail, cellSize, nil, nil, nil, nil, nil, {
         drawFace = false,
-        sharpCorners = true,
-        cornerCaps = true,
       })
 
       x = x + (3 * cellSize) + spacing
