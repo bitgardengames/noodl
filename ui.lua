1de35c1b010a56bbbb35d4220a93141f71a0b0b7
bitgardengames <bitgardengames@gmail.com>
2025-10-22 22:14:11 -0600

    Adjust UI outlines and highlight

--- ui.lua
+++ ui.lua
@@ -491,7 +491,7 @@ function UI.drawPanel(x, y, w, h, opts)
 	if opts.border ~= false then
 		local borderColor = opts.borderColor or UI.colors.border or UI.colors.panelBorder
 		setColor(borderColor, alphaMultiplier)
-		love.graphics.setLineWidth(opts.borderWidth or 2)
+		love.graphics.setLineWidth(opts.borderWidth or 3)
 		love.graphics.rectangle("line", x, y, w, h, radius, radius)
 		love.graphics.setLineWidth(1)
 	end
@@ -499,7 +499,7 @@ function UI.drawPanel(x, y, w, h, opts)
 	if opts.focused then
 		local focusRadius = radius + (opts.focusRadiusOffset or 4)
 		local focusPadding = opts.focusPadding or 3
-		local focusColor = opts.focusColor or UI.colors.border or UI.colors.highlight
+		local focusColor = opts.focusColor or UI.colors.highlight or UI.colors.border
 		setColor(focusColor, opts.focusAlpha or 1.1)
 		love.graphics.setLineWidth(opts.focusWidth or 3)
 		love.graphics.rectangle("line", x - focusPadding, y - focusPadding, w + focusPadding * 2, h + focusPadding * 2, focusRadius, focusRadius)
@@ -681,7 +681,7 @@ function UI.drawButton(id)
 
 	if UI.colors.border then
 		setColor(UI.colors.border)
-		love.graphics.setLineWidth(2)
+		love.graphics.setLineWidth(3)
 		love.graphics.rectangle("line", b.x, b.y + yOffset, b.w, b.h, radius, radius)
 	end
 
@@ -690,7 +690,7 @@ function UI.drawButton(id)
 		if focusStrength > 0.01 then
 			local focusRadius = radius + 4
 			local padding = 3
-			local focusColor = UI.colors.border or UI.colors.highlight
+			local focusColor = UI.colors.highlight or UI.colors.border
 			setColor(focusColor, 0.8 + 0.4 * focusStrength)
 			love.graphics.setLineWidth(3)
 			love.graphics.rectangle("line", b.x - padding, b.y + yOffset - padding, b.w + padding * 2, b.h + padding * 2, focusRadius, focusRadius)
