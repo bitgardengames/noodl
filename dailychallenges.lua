b064ac1ad7e524e19d0f195156bc93828fd258f0
BitGarden <bitgardengames@gmail.com>
2025-11-19 20:42:52 -0700

    daily tweak

--- dailychallenges.lua
+++ dailychallenges.lua
@@ -488,6 +488,7 @@ DailyChallenges.challenges = {
 		id = "floor_speedrunner",
 		titleKey = "menu.daily.floor_speedrunner.title",
 		descriptionKey = "menu.daily.floor_speedrunner.description",
+		sessionStat = "fastestFloorClear",
 		goal = 1,
 		progressKey = "menu.daily.floor_speedrunner.progress",
 		completeKey = "menu.daily.floor_speedrunner.complete",
