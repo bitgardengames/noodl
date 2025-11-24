54706f18c9139ad54aae6cda33cae38138785cb5
bitgardengames <bitgardengames@gmail.com>
2025-11-24 14:55:53 -0700

    Use array-backed hazard contexts

---
+++ movementcontext.lua
@@ -0,0 +1,14 @@
+local MovementContext = {
+        PUSH_X = 1,
+        PUSH_Y = 2,
+        SNAP_X = 3,
+        SNAP_Y = 4,
+        DIR_X = 5,
+        DIR_Y = 6,
+        GRACE = 7,
+        SHAKE = 8,
+        DAMAGE = 9,
+        INFLICTED_DAMAGE = 10,
+}
+
+return MovementContext
