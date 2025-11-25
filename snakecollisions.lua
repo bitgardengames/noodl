f3b611197313ccc9016473049be6ca5f06a62799
bitgardengames <bitgardengames@gmail.com>
2025-11-24 19:20:52 -0700

    Optimize collision contexts to use arrays

--- snakecollisions.lua
+++ snakecollisions.lua
@@ -4,19 +4,28 @@ local max = math.max
 
 local SnakeCollisions = {}
 
+-- Use indexed slots so collision contexts stay array-backed in hot paths.
+local CTX_SNAKE = 1
+local CTX_TRAIL = 2
+local CTX_HEAD_X = 3
+local CTX_HEAD_Y = 4
+local CTX_GUARD_DISTANCE = 5
+local CTX_BODY_RADIUS = 6
+local CTX_CUT_EVENT = 7
+
 local function resetCollisionContext(context)
         if not context then
                 return
         end
 
-        context.snake = nil
-        context.trail = nil
-        context.headX = nil
-        context.headY = nil
-        context.guardDistance = 0
-        context.bodyRadius = 0
+        context[CTX_SNAKE] = nil
+        context[CTX_TRAIL] = nil
+        context[CTX_HEAD_X] = nil
+        context[CTX_HEAD_Y] = nil
+        context[CTX_GUARD_DISTANCE] = 0
+        context[CTX_BODY_RADIUS] = 0
 
-        local cutEvent = context.cutEvent
+        local cutEvent = context[CTX_CUT_EVENT]
         if cutEvent then
                 cutEvent.index = nil
                 cutEvent.cutX = nil
@@ -37,12 +46,12 @@ local function prepareCollisionContext(context, snake, trail, guardDistance, bod
                 return false
         end
 
-        context.snake = snake
-        context.trail = trail
-        context.headX = headX
-        context.headY = headY
-        context.guardDistance = guardDistance
-        context.bodyRadius = bodyRadius
+        context[CTX_SNAKE] = snake
+        context[CTX_TRAIL] = trail
+        context[CTX_HEAD_X] = headX
+        context[CTX_HEAD_Y] = headY
+        context[CTX_GUARD_DISTANCE] = guardDistance
+        context[CTX_BODY_RADIUS] = bodyRadius
         return true
 end
 
@@ -61,43 +70,25 @@ function SnakeCollisions.new(deps)
         local Saws = deps.Saws
 
         local laserCollisionContext = {
-                snake = nil,
-                trail = nil,
-                headX = nil,
-                headY = nil,
-                guardDistance = 0,
-                bodyRadius = 0,
-                cutEvent = {cause = "laser"},
+                [CTX_CUT_EVENT] = {cause = "laser"},
         }
 
         local dartCollisionContext = {
-                snake = nil,
-                trail = nil,
-                headX = nil,
-                headY = nil,
-                guardDistance = 0,
-                bodyRadius = 0,
-                cutEvent = {cause = "dart"},
+                [CTX_CUT_EVENT] = {cause = "dart"},
         }
 
         local sawCollisionContext = {
-                snake = nil,
-                trail = nil,
-                headX = nil,
-                headY = nil,
-                guardDistance = 0,
-                bodyRadius = 0,
-                cutEvent = {},
+                [CTX_CUT_EVENT] = {},
         }
 
         local function evaluateTrailRectCut(context, expandedX, expandedY, expandedW, expandedH, candidateIndices, candidateCount, candidateLookup, candidateGeneration)
-                local trailRef = context.trail
+                local trailRef = context[CTX_TRAIL]
                 if not (trailRef and trailRef[1]) then
                         return nil
                 end
 
-                local headX = context.headX
-                local headY = context.headY
+                local headX = context[CTX_HEAD_X]
+                local headY = context[CTX_HEAD_Y]
                 if not (headX and headY) then
                         return nil
                 end
@@ -139,8 +130,8 @@ function SnakeCollisions.new(deps)
 
                                                 if intersects and t then
                                                         local along = travelled + segLen * t
-                                                        if along > context.guardDistance then
-                                                                local cutEvent = context.cutEvent
+                                                        if along > context[CTX_GUARD_DISTANCE] then
+                                                                local cutEvent = context[CTX_CUT_EVENT]
                                                                 cutEvent.index = index
                                                                 cutEvent.cutX = cutX
                                                                 cutEvent.cutY = cutY
@@ -160,13 +151,13 @@ function SnakeCollisions.new(deps)
         end
 
         local function evaluateTrailCircleCut(context, saw, centerX, centerY, combinedRadiusSq, candidateIndices, candidateCount, candidateLookup, candidateGeneration)
-                local trailRef = context.trail
+                local trailRef = context[CTX_TRAIL]
                 if not (trailRef and trailRef[1]) then
                         return nil
                 end
 
-                local headX = context.headX
-                local headY = context.headY
+                local headX = context[CTX_HEAD_X]
+                local headY = context[CTX_HEAD_Y]
                 if not (headX and headY) then
                         return nil
                 end
@@ -187,7 +178,7 @@ function SnakeCollisions.new(deps)
 
                 local travelled = 0
                 local prevX, prevY = headX, headY
-                local bodyRadius = context.bodyRadius or 0
+                local bodyRadius = context[CTX_BODY_RADIUS] or 0
 
                 for index = 2, #trailRef do
                         local segment = trailRef[index]
@@ -213,9 +204,9 @@ function SnakeCollisions.new(deps)
                                                 if candidate then
                                                         local closestX, closestY, distSq, t = closestPointOnSegment(centerX, centerY, prevX, prevY, cx, cy)
                                                         local along = travelled + segLen * (t or 0)
-                                                        if along > context.guardDistance and distSq <= combinedRadiusSq then
+                                                        if along > context[CTX_GUARD_DISTANCE] and distSq <= combinedRadiusSq then
                                                                 if isSawCutPointExposed(saw, centerX, centerY, closestX, closestY) then
-                                                                        local cutEvent = context.cutEvent
+                                                                        local cutEvent = context[CTX_CUT_EVENT]
                                                                         cutEvent.index = index
                                                                         cutEvent.cutX = closestX
                                                                         cutEvent.cutY = closestY
@@ -261,7 +252,7 @@ function SnakeCollisions.new(deps)
                         return false
                 end
 
-                local bodyRadius = context.bodyRadius
+                local bodyRadius = context[CTX_BODY_RADIUS]
 
                 for index = 1, emitterCount do
                         local beam = emitters[index]
@@ -277,7 +268,7 @@ function SnakeCollisions.new(deps)
 
                                                 local candidates, candidateCount, candidateLookup, candidateGeneration = collectSnakeSegmentCandidatesForRect(expandedX, expandedY, expandedW, expandedH)
                                                 local cutEvent = evaluateTrailRectCut(context, expandedX, expandedY, expandedW, expandedH, candidates, candidateCount, candidateLookup, candidateGeneration)
-                                                if cutEvent and context.snake:handleSawBodyCut(cutEvent) then
+                                                if cutEvent and context[CTX_SNAKE]:handleSawBodyCut(cutEvent) then
                                                         beam.burnAlpha = 0.92
                                                         resetCollisionContext(context)
                                                         return true
@@ -317,7 +308,7 @@ function SnakeCollisions.new(deps)
                         return false
                 end
 
-                local bodyRadius = context.bodyRadius
+                local bodyRadius = context[CTX_BODY_RADIUS]
 
                 for index = 1, emitterCount do
                         local emitter = emitters[index]
@@ -333,7 +324,7 @@ function SnakeCollisions.new(deps)
 
                                                 local candidates, candidateCount, candidateLookup, candidateGeneration = collectSnakeSegmentCandidatesForRect(expandedX, expandedY, expandedW, expandedH)
                                                 local cutEvent = evaluateTrailRectCut(context, expandedX, expandedY, expandedW, expandedH, candidates, candidateCount, candidateLookup, candidateGeneration)
-                                                if cutEvent and context.snake:handleSawBodyCut(cutEvent) then
+                                                if cutEvent and context[CTX_SNAKE]:handleSawBodyCut(cutEvent) then
                                                         resetCollisionContext(context)
                                                         return true
                                                 end
@@ -371,7 +362,7 @@ function SnakeCollisions.new(deps)
                         return false
                 end
 
-                local bodyRadius = context.bodyRadius
+                local bodyRadius = context[CTX_BODY_RADIUS]
 
                 for i = 1, #saws do
                         local saw = saws[i]
@@ -383,7 +374,7 @@ function SnakeCollisions.new(deps)
                                         if combined > 0 then
                                                 local candidates, candidateCount, candidateLookup, candidateGeneration = collectSnakeSegmentCandidatesForCircle(sx, sy, combined)
                                                 local cutEvent = evaluateTrailCircleCut(context, saw, sx, sy, combined * combined, candidates, candidateCount, candidateLookup, candidateGeneration)
-                                                if cutEvent and context.snake:handleSawBodyCut(cutEvent) then
+                                                if cutEvent and context[CTX_SNAKE]:handleSawBodyCut(cutEvent) then
                                                         resetCollisionContext(context)
                                                         return true
                                                 end
